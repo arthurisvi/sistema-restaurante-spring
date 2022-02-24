e383f4546673cf9d53c2cc5983758f1b861a9af9
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-02-24 20:33:09 -0300

    implementing repository extends Jpa

--- ms-payments/src/main/java/com/restaurantsystem/mspayments/repositories/PaymentRepository.java
+++ ms-payments/src/main/java/com/restaurantsystem/mspayments/repositories/PaymentRepository.java
@@ -1,5 +1,7 @@
 package com.restaurantsystem.mspayments.repositories;
 
-public class PaymentRepository {
-    
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.restaurantsystem.mspayments.entities.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long>{}
