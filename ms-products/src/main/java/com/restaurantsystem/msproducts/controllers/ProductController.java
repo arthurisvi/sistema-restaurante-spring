fb6cec9e318f6009457c19ab369507a62eb482db
pedro470 <77297069+pedro470@users.noreply.github.com>
2022-03-05 12:01:34 -0300

    Update ProductController.java
    Troca para controlador rest

--- ms-products/src/main/java/com/restaurantsystem/msproducts/controllers/ProductController.java
+++ ms-products/src/main/java/com/restaurantsystem/msproducts/controllers/ProductController.java
@@ -9,18 +9,18 @@ import com.restaurantsystem.msproducts.entities.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@Controller
+@RestController
 @RequestMapping("/products")
 public class ProductController {
     
