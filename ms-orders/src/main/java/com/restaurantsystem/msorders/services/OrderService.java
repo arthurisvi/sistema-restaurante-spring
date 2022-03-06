33287c069a4bfd9f5db0ecc8dbc8b2b2747d3f89
pedro470 <77297069+pedro470@users.noreply.github.com>
2022-03-06 00:36:00 -0300

    Update OrderService.java
    Integração de order com payment

--- ms-orders/src/main/java/com/restaurantsystem/msorders/services/OrderService.java
+++ ms-orders/src/main/java/com/restaurantsystem/msorders/services/OrderService.java
@@ -2,18 +2,25 @@ package com.restaurantsystem.msorders.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.restaurantsystem.msorders.entities.Order;
+import com.restaurantsystem.msorders.entities.Payment;
+import com.restaurantsystem.msorders.feignClients.PaymentFeignClient;
 import com.restaurantsystem.msorders.repositories.OrderRepository;
 
 @Service
 public class OrderService {
 	private OrderRepository orderRepository;
+
 	
 	public OrderService(OrderRepository orderRepository) {	
 		this.orderRepository = orderRepository;
 	}
+
+	@Autowired
+	PaymentFeignClient paymentController;
 	
 	public List<Order> getAllOrders() {
 		List<Order> result = orderRepository.findAll();
@@ -26,6 +33,9 @@ public class OrderService {
 	}
 	
 	public Order createOrder(Order newOrder) {
+		Payment payment = paymentController.createPayment(newOrder.getPayment());
+		//paymentController.findById(payment.getId());
+		newOrder.setPayment(payment);
 		Order order = orderRepository.save(newOrder);
 		
 		return order;
