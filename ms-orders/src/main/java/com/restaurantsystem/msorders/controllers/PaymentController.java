e1f71c533d6729ba23c312a3e8fbfeeb3f070b85
pedro470 <77297069+pedro470@users.noreply.github.com>
2022-03-05 23:23:02 -0300

    payment controller
    Adição de um autowired para a correta injeção de dependência

--- ms-orders/src/main/java/com/restaurantsystem/msorders/controllers/PaymentController.java
+++ ms-orders/src/main/java/com/restaurantsystem/msorders/controllers/PaymentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.restaurantsystem.msorders.entities.Payment;
 import com.restaurantsystem.msorders.feignClients.PaymentFeignClient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("payments")
+@RequestMapping("/payments")
 public class PaymentController {
     
+    @Autowired
     private PaymentFeignClient paymentClient;
 
     @GetMapping
