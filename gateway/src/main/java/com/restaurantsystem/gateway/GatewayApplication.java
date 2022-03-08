af5cf8b75a4a34e34443523b9727c45ad83addcf
pedro470 <77297069+pedro470@users.noreply.github.com>
2022-03-08 01:10:42 -0300

    Gateway funcional
    Com interface zero, mas acessando todas as outras APIs facilmente.

--- gateway/src/main/java/com/restaurantsystem/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/restaurantsystem/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.restaurantsystem.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//@EnableEurekaServer
 public class GatewayApplication {
 
 	public static void main(String[] args) {
